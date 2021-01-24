852d453b6953fc76b2624c5b650d718e8e3f6002
Tim Deeb-Swihart <tim@deebswih.art>
2021-01-23 19:47:17 -0500

    rig up a test harness

--- src/bin/kernel.rs
+++ src/bin/kernel.rs
@@ -1,10 +1,14 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(firstos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use firstos::{eprint, println};
 
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     eprint!("{}", info);
@@ -17,5 +21,12 @@ const MSG: &str = "We've booted! Hooray!";
 pub extern "C" fn _start() -> ! {
     println!("{}", MSG);
     panic!("error handling!");
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    eprint!("testing {}", info);
     loop {}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,30 @@
 #![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 pub mod vga;
+
+use core::panic::PanicInfo;
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    eprint!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    print!("test harness?");
+    loop {}
+}
+
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
