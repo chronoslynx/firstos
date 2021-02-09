0cf2529b9c191fccad54a65e5e4b8138e65e0d7b
Tim Deeb-Swihart <tim@deebswih.art>
2021-02-08 21:34:00 -0500

    Test for stack overflows and GDT setup

--- src/gdt.rs
+++ src/gdt.rs
@@ -3,12 +3,12 @@ use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor, SegmentSelector
 use x86_64::structures::tss::TaskStateSegment;
 use lazy_static::lazy_static;
 
-pub const DOUBLE_FAULT_INDEX: u16 = 0;
+pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 
 lazy_static! {
     static ref TSS: TaskStateSegment = {
         let mut tss = TaskStateSegment::new();
-        tss.interrupt_stack_table[DOUBLE_FAULT_INDEX as usize] = {
+        tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
             const STACK_SIZE: usize = 4096 * 5;
             static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
             let stack_start = VirtAddr::from_ptr(unsafe{ &STACK });
